ca3e022f7dce8f3b60ee30c1278209b1044ea3ae
Paul Soule <22938608+pds2208@users.noreply.github.com>
2020-03-02 17:46:38 +0000

    WIP

--- messaging/connection.go
+++ messaging/connection.go
@@ -1,73 +1,77 @@
 package messaging
 
 import (
-    "encoding/json"
-    "github.com/rs/zerolog/log"
-    "github.com/streadway/amqp"
+	"encoding/json"
+	"github.com/rs/zerolog/log"
+	"github.com/streadway/amqp"
 )
 
 type Messaging struct {
-    Connection amqp.Connection
+	Connection amqp.Connection
 }
 
 func NewConnection() *Messaging {
-    a := &Messaging{}
-    a.Connect()
-    return a
+	a := &Messaging{}
+	a.Connect()
+	return a
 }
 
 func (m Messaging) Connect() {
-    conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-    failOnError(err, "Failed to connect to RabbitMQ")
-    defer conn.Close()
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Err(err).
+			Msg("Cannot connect to RabbitMQ")
+		return
+	}
+	defer conn.Close()
 
-    ch, err := conn.Channel()
-    failOnError(err, "Failed to open a channel")
-    defer ch.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-    msgs, err := ch.Consume(
-        "lfs-postcode", // queue
-        "",             // consumer
-        true,           // auto-ack
-        false,          // exclusive
-        false,          // no-local
-        false,          // no-wait
-        nil,            // args
-    )
+	msgs, err := ch.Consume(
+		"lfs-postcode", // queue
+		"",             // consumer
+		true,           // auto-ack
+		false,          // exclusive
+		false,          // no-local
+		false,          // no-wait
+		nil,            // args
+	)
 
-    failOnError(err, "Failed to register a consumer")
+	failOnError(err, "Failed to register a consumer")
 
-    forever := make(chan bool)
+	forever := make(chan bool)
 
-    go func() {
-        for d := range msgs {
-            handleMessage(d.Body)
-        }
-    }()
+	go func() {
+		for d := range msgs {
+			handleMessage(d.Body)
+		}
+	}()
 
-    log.Info().Msg("Waiting for messages...")
-    <-forever
+	log.Info().Msg("Waiting for messages...")
+	<-forever
 }
 
 func handleMessage(message []byte) {
-    res := Message{}
-    err := json.Unmarshal([]byte(message), &res)
-    if err != nil {
-        log.Error().Err(err)
-        return
-    }
-    log.Printf("Received request for year: %d, month %d", res.Year, res.Month)
+	res := Message{}
+	err := json.Unmarshal([]byte(message), &res)
+	if err != nil {
+		log.Error().Err(err)
+		return
+	}
+	log.Printf("Received request for year: %d, month %d", res.Year, res.Month)
 }
 
 func failOnError(err error, msg string) {
-    if err != nil {
-        log.Fatal().
-            Err(err).
-            Str("Rabbit MQ", "Error")
-    }
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("Rabbit MQ", "Error")
+	}
 }
 
 type Message struct {
-    Month int `json:"month"`
-    Year  int `json:"year"`
+	Month int `json:"month"`
+	Year  int `json:"year"`
 }
